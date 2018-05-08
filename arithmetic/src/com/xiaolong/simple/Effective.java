436e3883c2df9bb63acb2083243f865f0f88f8f6
somedragon <296569845@qq.com>
2018-05-08 11:43:00 +0800

    some file

--- arithmetic/src/com/xiaolong/simple/Effective.java
+++ arithmetic/src/com/xiaolong/simple/Effective.java
@@ -1,10 +1,18 @@
 package com.xiaolong.simple;
 
 /**
- * 
+ *  有效的括号
+ *  给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *  有效字符串需满足：
+ *      左括号必须用相同类型的右括号闭合。
+ *      左括号必须以正确的顺序闭合。
+ *      注意空字符串可被认为是有效字符串。
+ *      示例 1:
+ *          输入: "()"
+ *          输出: true
  */
 public class Effective  {
-
+    
     public boolean isValid(String s) {
        char[] chars = s.toCharArray(), schars = new char[s.length()];
        int top = 0, len = s.length();
