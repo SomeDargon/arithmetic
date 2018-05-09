a43f0230722e5eea965facebd96c8a2de37918ed
somedragon <296569845@qq.com>
2018-05-09 16:36:00 +0800

    update file

--- arithmetic/src/com/xiaolong/simple/ListNode.java
+++ arithmetic/src/com/xiaolong/simple/ListNode.java
@@ -21,6 +21,7 @@ class Solution {
 
     /**
      * 这个是遍历方法还有一张是递归方法
+     *
      * @param l1
      * @param l2
      * @return
@@ -28,40 +29,45 @@ class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
         //这里很简单，如果那个为空，就返回对应的那个
-        if(l1==null) {return l2;}
-        if(l2==null) {return l1;}
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
 
         //连接第一个头部
-        ListNode oldList = l1, newList = l2 , head , o;
+        ListNode oldList = l1, newList = l2, head, o;
 
         //这里是为了获取头结点， 那个小那个就放在前面
-        if(oldList.val >= newList.val){
+        if (oldList.val >= newList.val) {
             head = o = newList;
             newList = newList.next;
-        }else {
+        } else {
             head = o = oldList;
             oldList = oldList.next;
         }
-        while (oldList!=null && newList!=null){
-            if(oldList.val>newList.val){
+        while (oldList != null && newList != null) {
+            if (oldList.val > newList.val) {
                 //这注意一下.三个步骤
                 //1.先把最小的那个，放到自定义链表o的下一个
                 //2.然后保证o指向的是最后一个节点
                 //3.最后把小的那个节点指向下一个
                 o = o.next = newList;
                 newList = newList.next;
-            }else {
+            } else {
                 o = o.next = oldList;
                 oldList = oldList.next;
             }
         }
 
+
         //最后是遍历没有遍历完的，直接添加到后面去
-        while (oldList != null){
+        while (oldList != null) {
             o = o.next = oldList;
             oldList = oldList.next;
         }
-        while (newList != null){
+        while (newList != null) {
             o = o.next = newList;
             newList = newList.next;
         }
